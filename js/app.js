51594ca8a64be1c8b412ac8907bf44ba82607b31
Kevin Lin <whatakevin@gmail.com>
2016-03-02 02:22:15 -0600

    get access token

--- js/app.js
+++ js/app.js
@@ -7,11 +7,24 @@ angular.module('myApp', [
     $locationProvider.html5Mode(true);
 }])
 
-.controller('indexController', ['$scope', '$location', '$window', function($scope, $location, $window) {
+.controller('indexController', ['$scope', '$location', '$window', '$http', function($scope, $location, $window, $http) {
     $scope.title = "Title";
     $scope.urlParams = $location.search();
-    var code = $location.search().code;
-    if(typeof code === 'undefined'){
-        $window.location.href = 'https://www.clover.com/oauth/authorize?client_id=WXSGVB6YKHQKG';
+    var urlParams = $location.search();
+    if(typeof urlParams.merchant_id === 'undefined'){
+        $window.location.href = 'https://www.clover.com/oauth/authorize?client_id=WXSGVB6YKHQKG&response_type=token';
     }
+    var accessToken = $location.url().slice($location.url().indexOf("#access_token=") + "#access_token=".length);
+    var orderId = '6DFFA6XAAAH58';
+    $http({
+        method: 'GET',
+        url: 'https://api.clover.com/v3/merchants/' + urlParams.merchant_id + '/orders/' + orderId,
+        headers: {
+            'Authorization' : 'Bearer ' + accessToken
+        }
+    }).then(function successCallback(response) {
+        console.log(response);
+    }, function errorCallback(response) {
+        console.log(response);
+    });
 }]);
