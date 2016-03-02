09b2f937d0678c0944f75156c732c90d072629c0
Kevin Lin <whatakevin@gmail.com>
2016-03-02 00:59:59 -0600

    Add oauth check

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@ angular.module('myApp', [
     'ngRoute'
 ])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.otherwise({redirectTo: '/index.html'});
+    $locationProvider.html5Mode(true);
 }])
 
-.controller('indexController', ['$scope', function($scope) {
+.controller('indexController', ['$scope', '$location', '$window', function($scope, $location, $window) {
     $scope.title = "Title";
+    $scope.urlParams = $location.search();
+    var code = $location.search().code;
+    if(typeof code === 'undefined'){
+        $window.location.href = 'https://www.clover.com/oauth/authorize?client_id=WXSGVB6YKHQKG';
+    }
 }]);
